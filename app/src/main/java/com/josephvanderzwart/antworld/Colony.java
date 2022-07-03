0252621aa339e7972d1cf86ab66f1666ce181211
joseph243 <josephvanderzwart@protonmail.com>
2022-07-03 12:29:42 -0600

    message/handler first

--- app/src/main/java/com/josephvanderzwart/antworld/Colony.java
+++ app/src/main/java/com/josephvanderzwart/antworld/Colony.java
@@ -4,15 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.TextView;
 
 public class Colony extends AppCompatActivity {
+
+    private TextView textView;
+
+    private static Handler handler;
+
+    public static Handler getHandler(){
+        return handler;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colony);
         getSupportActionBar().hide();
+
+        //counter text view:
+        textView = (TextView) findViewById(R.id.counter);
+
+        //init handler for posting message:
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg){
+                textView.setText(msg.getData().get("key").toString());
+            }
+        };
     }
 
     public void onPictureView(View view) {
--- app/src/main/java/com/josephvanderzwart/antworld/GameRunner.java
+++ app/src/main/java/com/josephvanderzwart/antworld/GameRunner.java
@@ -1,10 +1,14 @@
 package com.josephvanderzwart.antworld;
 
+import android.os.Bundle;
+import android.os.Message;
+
 public class GameRunner implements Runnable {
 
     private volatile boolean running = true;
     private int i = 0;
     private Thread thread;
+    public Message message;
 
     public void run() {
         System.out.println("entering run");
@@ -12,6 +16,24 @@ public class GameRunner implements Runnable {
             i++;
             try {
                 System.out.println("running: " + i);
+
+                //handler attempt
+                while (message == null) {
+                    try {
+                        message = Colony.getHandler().obtainMessage();
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+                Bundle bundle = new Bundle();
+                bundle.putString("key", "Ants Count: " + i);
+                message.setData(bundle);
+                Colony.getHandler().sendMessage(message);
+                message = null;
+                //end attempt
+
                 Thread.sleep(1000);
             }
             catch (InterruptedException e) {
